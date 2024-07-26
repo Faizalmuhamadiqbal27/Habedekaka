e2004188d9deda8668f6ba2947d9bb41eff46bac
Sanjikun <117532465+Sanjikunnn@users.noreply.github.com>
2024-07-26 19:24:18 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
                 console.log("Audio element found:", audio); // Log debug
 
                 // Coba set currentTime sebelum play
-                audio.currentTime = 15;
+                audio.currentTime = 30;
                 var playPromise = audio.play();
                 if (playPromise !== undefined) {
                     playPromise.then(function() {
